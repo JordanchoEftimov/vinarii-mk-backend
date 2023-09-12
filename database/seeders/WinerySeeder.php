6e1b5dffae4ed1487df31d9b7cf1d801ecf2258e
borcesmileski <borche.smileski@students.finki.ukim.mk>
2023-09-12 10:17:41 +0200

    seeder fixed it

--- database/seeders/WinerySeeder.php
+++ database/seeders/WinerySeeder.php
@@ -3,9 +3,9 @@
 namespace Database\Seeders;
 
 use App\Enums\UserRole;
+use App\Models\User;
 use App\Models\Wine;
 use App\Models\Winery;
-use http\Client\Curl\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,21 +14,29 @@ class WinerySeeder extends Seeder
     /**
      * Run the database seeds.
      */
+
     public function run(): void
     {
-        $wineries = Winery::query()->get();
+        $wineryUsers = User::where('role', UserRole::WINERY->value)->get();
 
-        foreach ($wineries as $i => $winery) {
-            $winery->legal_name = 'Winery ' . ($i + 1);
-            $winery->logo_image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz_Y_xijF5v_1ZkbGhWSasHiHwMdgX6nOCVXUQU0XoxVauzu0jdBKkhNQA2sAT_9QI8V4&usqp=CAU';
-            $winery->cover_image = 'https://www.thetopvillas.com/blog/wp-content/uploads/2018/07/wine-featured.jpg';
-            $winery->address = 'Address ' . ($i + 1);
-            $winery->email = 'winery' . ($i + 1) . '@example.com';
-            $winery->phone = '123-456-789' . $i;
-            $winery->description = 'Description for Winery ' . ($i + 1);
-            $winery->working_hours = 'Working Hours ' . ($i + 1);
+        for ($i = 0; $i < 10; $i++) {
 
-            $winery->save();
+            $user = $wineryUsers->random();
+
+            $wineryData = [
+                'legal_name' => 'Winery ' . ($i + 1),
+                'user_id' => $user->id,
+                'logo_image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz_Y_xijF5v_1ZkbGhWSasHiHwMdgX6nOCVXUQU0XoxVauzu0jdBKkhNQA2sAT_9QI8V4&usqp=CAU',
+                'cover_image' => 'https://www.thetopvillas.com/blog/wp-content/uploads/2018/07/wine-featured.jpg',
+                'address' => 'Address ' . ($i + 1),
+                'email' => 'winery' . ($i + 1) . '@example.com',
+                'phone' => '123-456-789' . $i,
+                'description' => 'Description for Winery ' . ($i + 1),
+                'working_hours' => 'Working Hours ' . ($i + 1),
+            ];
+
+            // Create the winery
+            Winery::create($wineryData);
         }
     }
 }
