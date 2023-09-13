2ba50b6446147765db1fc2809db1674c4caaaa16
JordanchoEftimov <ejordancho@gmail.com>
2023-09-13 19:55:57 +0200

    winery setting policy

--- app/Filament/Resources/WineryResource/RelationManagers/WinerySettingsRelationManager.php
+++ app/Filament/Resources/WineryResource/RelationManagers/WinerySettingsRelationManager.php
@@ -34,19 +34,17 @@ class WinerySettingsRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                //
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                //
             ])
             ->emptyStateActions([
-                Tables\Actions\CreateAction::make(),
+                //
             ]);
     }
 }
